fc5d27008e1c0e3866eb48ae859050d49652a9fd
micpob <bomboloapp@gmail.com>
2023-10-03 08:37:28 +0200

    Exercise 9.7

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { isNotNumber } from "./utils";
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
+
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -16,6 +19,24 @@ app.get('/bmi', (req, res) => {
     const bmi = calculateBmi(height, weight);  
     res.send({height: height, weight: weight, bmi: bmi});
   }
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const dailyTargetHours : any = req.body.target;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const dailyExerciseHours : any = req.body.daily_exercises;
+
+  if (typeof dailyTargetHours == 'undefined' || typeof dailyExerciseHours == 'undefined') {
+    res.status(404).send({ error: "parameters missing" });
+  } else if (isNotNumber(dailyTargetHours) || dailyExerciseHours.some(isNotNumber)) {
+    res.status(404).send({ error: "malformatted parameters" });
+  } else {
+    const result = calculateExercises(dailyTargetHours, dailyExerciseHours);
+    console.log(result);
+    res.json(result);
+  }  
+  
 });
 
 const PORT = 3002;
